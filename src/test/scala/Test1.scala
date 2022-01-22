0c01c26af8d4b6f8acf1da4eb270fdec68161e79
Krzysztophoros <krisdabrowski1992@gmail.com>
2022-01-22 13:30:20 +1100

    Add tests in main branch for navigation to testRoom2

--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -9,14 +9,19 @@ val testLookString = "It looks like a test!"
 val testSmellString = "It smells like a test!"
 val testItem = Item("test",SenseProps("It looks like a test!","It smells like a test!"))
 
+val testChicken = Item("chicken",SenseProps("It looks like a chicken!","It smells like a chicken!"))
+
+val testRoom2 = Room("testRoom2", "This looks like the second testing room.", List(testChicken))
+
 val testRoom = Room("testRoom", "This looks like the testing room.", List(testItem))
-val testDirectory = RoomDirectory(List((testRoom,List())))
+val testDirectory = RoomDirectory(List((testRoom,List(testRoom2))))
 
 class Test1:
   @Test def t1(): Unit = 
     assertEquals("I was compiled by Scala 3. :)", msg)
 
 class Look_Tests:
+  println("TESTING FIRST BATCH")
   var state = State("",(testRoom, testDirectory))
   @Test def t1(): Unit = 
       val input = "look room"
@@ -35,4 +40,11 @@ class Look_Tests:
       val expectedText = testItem.sense(ValidCommand.Smell)
       assertEquals(newState.tempData, expectedText)
       assertEquals(newState.tempData, testSmellString)
+  @Test def t4(): Unit = 
+      val input = "go testRoom2"
+      val newState = roomState_loop(input,state)
+      val expectedText = s"You go to the ${testRoom2.name}\n" + testRoom2.description
+      assertEquals(newState.tempData, expectedText)
+      val newerState = roomState_loop("look chicken", newState)
+      assertEquals(newerState.tempData, testChicken.senseProps.look)
 
